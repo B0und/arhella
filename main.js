5527a8705428429b927448ec255b87b23aa399db
Unbound <unbound9dev@gmail.com>
2022-08-27 19:44:45 +0300

    add nav highlight

---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id")
+      if (entry.intersectionRatio > 0.99) {
+        document.querySelector(`nav a[href="#${id}"]`).classList.add("active-nav")
+      } else {
+        document.querySelector(`nav a[href="#${id}"]`).classList.remove("active-nav")
+      }
+    })
+  })
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("span[id]").forEach((section) => {
+    observer.observe(section)
+  })
+})
