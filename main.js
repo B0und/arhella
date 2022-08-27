81c6ed9463f15ff2a562556c9120dfd0ac01bc58
Unbound <unbound9dev@gmail.com>
2022-08-27 20:17:35 +0300

    upd script

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       const id = entry.target.getAttribute("id")
-      if (entry.intersectionRatio > 0.99) {
+      if (entry.intersectionRatio > 0) {
         document.querySelector(`nav a[href="#${id}"]`).classList.add("active-nav")
       } else {
         document.querySelector(`nav a[href="#${id}"]`).classList.remove("active-nav")
